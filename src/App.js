5f037d8b60060cc14df92c02dc291e00d78e3a27
manjunath1818 <57515636+manjunath1818@users.noreply.github.com>
2020-09-09 09:14:34 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Manju123UPDTAE111!
+          Hi Manju123UPDTAE111Manj!
         </p>
         <a
           className="App-link"
